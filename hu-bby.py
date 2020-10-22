87c7cdc926110f97d8a97e3c0c6939cf14943538
nazlıcan <72741991+nazlcan666@users.noreply.github.com>
2020-10-23 00:20:48 +0300

    Create hu-bby.py

---
+++ hu-bby.py
@@ -0,0 +1,12 @@
+def hesapla(kelime, paragraf):
+  array = paragraf.split()
+  toplam=0
+  for klm in array:
+      if klm== kelime:
+          toplam+=1
+  return toplam
+
+text = 'Bir gün, istiklâl ve cumhuriyeti müdafaa mecburiyetine düşersen, vazifeye atılmak için, içinde bulunacağın vaziyetin imkân ve şeraitini düşünmeyeceksin! Bu imkân ve şerait, çok nâmüsait bir mahiyette tezahür edebilir. İstiklâl ve cumhuriyetine kastedecek düşmanlar, bütün dünyada emsali görülmemiş bir galibiyetin mümessili olabilirler.'
+arama_sonucu = hesapla('ve', text)
+
+print(arama_sonucu)
